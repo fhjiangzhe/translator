1bc93416a269351fd3eabb5944134867430213e7
Jiang <ownfigo@gmail>
2023-09-17 19:35:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#大语言模型翻译PDF文件
+
+##模型：gpt-3.5-turbo
+
+##执行：python3 ai_translator/main.py
---
+++ ai_translator/main.py
@@ -0,0 +1,43 @@
+import gradio as gr
+import sys
+import os
+import shutil
+from translator import PDFTranslator
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
+def translate_pdf(file, source_lang, target_lang):
+    # 检查文件对象是否为空
+    if file is None:
+        return "未上传任何文件"
+
+    # 上传文件保存到指定目录
+    save_path = "saved_files"
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    destination = os.path.join(save_path, os.path.basename(file.name))
+    shutil.copy(file.name, destination)
+
+    # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
+    translator = PDFTranslator("gpt-3.5-turbo")
+    translated_file_path = translator.translate_pdf(destination, "markdown", source_lang, target_lang, pages=None)
+
+    return translated_file_path
+
+
+iface = gr.Interface(
+    fn=translate_pdf,
+    inputs=[
+        gr.inputs.File(label="上传待翻译PDF文件"),
+        gr.inputs.Dropdown(choices=['English', 'Chinese', 'Spanish'], default='English', label="源语言"),
+        gr.inputs.Dropdown(choices=['English', 'Chinese', 'Spanish'], default='Chinese', label="目标语言")
+    ],
+    outputs=[
+        gr.outputs.File(label="下载翻译后的文件")
+    ],
+    live=False
+)
+
+iface.launch(share=True, server_name="0.0.0.0")
